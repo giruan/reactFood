9d25981c3b6be2fb6f99c1f7ce87091513ae1da1
leessungkyu <lkyu63@naver.com>
2024-05-17 17:12:09 +0900

    찜목록 완

--- src/component/MyZzimPage.js
+++ src/component/MyZzimPage.js
@@ -48,29 +48,30 @@ function MyZzimPage(props) {
           return (
             <li key={i}>
               <div className="lookmyFavorites">
-                <div className="resName">
-                  {restaurantName.map((res, j) => {
-                    if (res.restaurantId === favorite.restaurantId) {
-                      return (
-                        <h2 className="resTitle" key={j}>
-                          <Link to={`/detail/${res.restaurantId}`}>{res.restaurantName}</Link>
-                        </h2>
-                      );
-                    }
-                    return null;
-                  })}
-                </div>
-                <div className="myReviewImg">
+                <div className="myZzimImg">
                   <img src={`/test/${matchingImg.imgUrl}`} className="col-3 title-img" />
                 </div>
-              </div>
-              <div className='zzim-box'>
-                <span className='zzimBtn' onClick={() => handleClick(favorite.restaurantId)}>
-                  찜하기
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="64" height="64">
-                    <path fill={zzimStates[favorite.restaurantId] ? "red" : "none"} stroke="black" strokeWidth="1" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                  </svg>
-                </span>
+                <div className='zzimBox'>
+                  <div className="ZzimResName">
+                    {restaurantName.map((res, j) => {
+                      if (res.restaurantId === favorite.restaurantId) {
+                        return (
+                          <h2 className="ZzimResTitle" key={j}>
+                            <Link to={`/detail/${res.restaurantId}`}>{res.restaurantName}</Link>
+                          </h2>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                  <div className='zzimBtn-box'>
+                    <span className='zzimBtn' onClick={() => handleClick(favorite.restaurantId)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+                        <path fill={zzimStates[favorite.restaurantId] ? "red" : "none"} stroke="black" strokeWidth="1" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
               </div>
             </li>
           );
